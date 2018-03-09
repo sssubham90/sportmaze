67ba3cb0e9a33a0d54efeff75349759673268cd4
sssubham90 <yoyosssubham@gmail.com>
2018-03-09 14:09:19 +0530

    Done

--- app/src/main/java/com/devil/sportmaze/VideoPlayerActivity.java
+++ app/src/main/java/com/devil/sportmaze/VideoPlayerActivity.java
@@ -16,8 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -29,6 +32,8 @@ public class VideoPlayerActivity extends AppCompatActivity {
     String key,name;
     FirebaseUser user;
     private ProgressDialog dialog;
+    private long dur;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,8 +88,8 @@ public class VideoPlayerActivity extends AppCompatActivity {
         Date c = Calendar.getInstance().getTime();
         SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
         String date = df.format(c);
-        String msg = getTime(videoView.getCurrentPosition())+"/"+getTime(videoView.getDuration());
-        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+        final String msg = getTime(videoView.getCurrentPosition())+"/"+getTime(videoView.getDuration());
+        myRef = FirebaseDatabase.getInstance().getReference();
         dialog = new ProgressDialog(this);
         dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         dialog.setMessage("Loading. Please wait...");
@@ -93,6 +98,16 @@ public class VideoPlayerActivity extends AppCompatActivity {
         dialog.show();
         if(user.getEmail()!=null){
             myRef.child("Users").child(user.getDisplayName()+"("+user.getEmail().substring(0,user.getEmail().indexOf('@'))+")").child(date).child(name).setValue(msg);
+            myRef.child("Users").child(user.getDisplayName()+"("+user.getEmail().substring(0,user.getEmail().indexOf('@'))+")").child("Total Duration").addListenerForSingleValueEvent(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(DataSnapshot dataSnapshot) {
+                       dur=dataSnapshot.getValue()!=null?Long.parseLong(dataSnapshot.getValue().toString()):0;
+                       myRef.child("Users").child(user.getDisplayName()+"("+user.getEmail().substring(0,user.getEmail().indexOf('@'))+")").child("Total Duration").setValue(dur+(videoView.getCurrentPosition()/60000));
+                   }
+                   @Override
+                   public void onCancelled(DatabaseError databaseError) {
+                   }
+            });
             myRef.child("Video").child(key).child("Viewers").child(user.getDisplayName()).setValue(msg).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
